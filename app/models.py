d9078dbb5f786320539db1b62342d913536dc00a
asha2525 <afroz25asha@gmail.com>
2024-06-29 23:34:17 +0600

    updated

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=80)
@@ -7,3 +7,4 @@ class Todo(models.Model):
     completed = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
